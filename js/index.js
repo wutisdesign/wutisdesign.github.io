ff212b1219b46ec6c5e1634a75538ba6e8fdde9a
wutisdesign <teresaw@BPPRODUCTION34.fios-router.home>
2017-03-27 07:58:30 -0400

    added pages

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 $(function(){
 
-  var counter = 0;
-  $(".message").hide();
+  // var counter = 0;
+  // $(".message").hide();
+
 
   $("#eel").click(function(event) {
   $("#eel").hide(); // on click hide image in the main container
     event.preventDefault();
     $( this ).hide();
     $( "#item1" ).show("slow"); // show item in image bank
-    counter = counter + 1;
-    console.log(counter) 
+    // counter = counter + 1;
+    // console.log(counter) 
 
   });
   
@@ -18,8 +19,8 @@ $(function(){
     event.preventDefault();
     $( this ).hide();
     $( "#item3" ).show("slow");
-    counter = counter + 1;
-    console.log(counter) 
+    // counter = counter + 1;
+    // console.log(counter) 
 
 
   });
@@ -29,19 +30,65 @@ $(function(){
     event.preventDefault();
     $( this ).hide();
     $( "#item2" ).show("slow");
-    counter = counter + 1;
-    console.log(counter) 
+    // counter = counter + 1;
+    // console.log(counter) 
 
 
   });
 
-  var choices = [ 'eel', 'octopus', 'leafyseadragon'];
-  
-  function checkAnswer(){
+  $("#owl").click(function(event) {
+  $("#owl").hide();
+    event.preventDefault();
+    $( this ).hide();
+    $("#item6").show("slow");
+    // counter = counter + 1;
+    // console.log(counter) 
+  });
 
-    // check if score = 3
-    // show message
-  }
+  $("#rabbit").click(function(event) {
+  $("#rabbit").hide();
+    event.preventDefault();
+    $( this ).hide();
+    $("#item5").show("slow");
+    // counter = counter + 1;
+    // console.log(counter) 
+  });
+
+  $("#arctic-fox").click(function(event) {
+  $("#arctic-fox").hide();
+    event.preventDefault();
+    $( this ).hide();
+    $("#item4").show("slow");
+    // counter = counter + 1;
+    // console.log(counter) 
+  });
+
+  $("#bird").click(function(event) {
+  $("#bird").hide();
+    event.preventDefault();
+    $( this ).hide();
+    $("#item7").show("slow");
+    // counter = counter + 1;
+    // console.log(counter) 
+  });
+
+  $("#stickbug").click(function(event) {
+  $("#stickbug").hide();
+    event.preventDefault();
+    $( this ).hide();
+    $("#item8").show("slow");
+    // counter = counter + 1;
+    // console.log(counter) 
+  });
+
+  $("#leafbug").click(function(event) {
+  $("#leafbug").hide();
+    event.preventDefault();
+    $( this ).hide();
+    $("#item9").show("slow");
+    // counter = counter + 1;
+    // console.log(counter) 
+  });
 
 
 });
